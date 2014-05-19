2a39cd630d8254487fffea663044211cb67c8512
Toni Petrina <toni@tonicodes.net>
2014-05-19 13:33:41 +0200

    Adding helper methods for writing MVVM properties

--- FastSharp/FastSharpIDE/ViewModel/ViewModelBase.cs
+++ FastSharp/FastSharpIDE/ViewModel/ViewModelBase.cs
@@ -12,15 +12,39 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
 namespace FastSharpIDE.ViewModel
 {
     public class ViewModelBase : GalaSoft.MvvmLight.ViewModelBase
     {
-        protected bool Set<T>(ref T field, T newValue, [CallerMemberName] string propertyName = "")
+        private readonly Dictionary<string, object> _properties = new Dictionary<string, object>();
+
+        protected new bool Set<T>(ref T field, T newValue, [CallerMemberName] string propertyName = "")
         {
             return Set(propertyName, ref field, newValue);
+        }
+
+        protected T Get<T>([CallerMemberName] string propertyName = "")
+        {
+            object value;
+            if (_properties.TryGetValue(propertyName, out value))
+                return (T)value;
+            return default(T);
+        }
+
+        protected bool Set<T>(T newValue, [CallerMemberName] string propertyName = "")
+        {
+            object value;
+            if (_properties.TryGetValue(propertyName, out value))
+            {
+                if (EqualityComparer<T>.Default.Equals((T)value, newValue))
+                    return false;
+            }
+
+            _properties[propertyName] = newValue;
+            return true;
         }
     }
 }
