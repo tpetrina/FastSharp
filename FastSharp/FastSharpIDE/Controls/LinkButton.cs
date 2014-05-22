7d6899504143cc040e941bb54b8488bcd65a7c3b
Toni Petrina <toni@tonicodes.net>
2014-05-22 14:05:00 +0200

    Adding links to the open source libraries on the about page

---
+++ FastSharp/FastSharpIDE/Controls/LinkButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using System.Windows.Controls;
+
+namespace FastSharpIDE.Controls
+{
+    public class LinkButton : Button
+    {
+        public LinkButton()
+        {
+            Click += LinkButton_Click;
+        }
+
+        void LinkButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Process.Start(Content as string);
+        }
+    }
+}
